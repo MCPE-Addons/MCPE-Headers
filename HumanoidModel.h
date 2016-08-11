c04b7661b74846c41a037a98584cbfc67c81cb41
Jose Vazquez <cabo.rambo@hotmail.com>
2016-08-10 22:40:03 -0500

    Forgot to commit this in my last push :P

--- HumanoidModel.h
+++ HumanoidModel.h
@@ -6,6 +6,8 @@ class GeometryPtr;
 
 class HumanoidModel : public Model {
 public:
+	char hmfiller1[1540];	// filling includes overriden classes
+	
 	virtual ~HumanoidModel();
 	virtual void render(Entity&, float, float, float, float, float, float);
 	virtual void setupAnim(float, float, float, float, float, float);
