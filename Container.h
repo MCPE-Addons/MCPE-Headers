ed6696dfbfa883cb72359628824082264bc1efaf
Jose Vazquez <cabo.rambo@hotmail.com>
2016-08-21 15:32:14 -0500

    Added CraftingContainer and ContainerType header, completed Container one

--- Container.h
+++ Container.h
@@ -1,45 +1,50 @@
 #pragma once
 
 #include <string>
+#include <vector>
+
+#include "ContainerType.h"
 
 class ContainerListener;
 class ItemInstance;
 class Random;
 class BlockSource;
 class Vec3;
-class CompoundTag;
 class Player;
+class CompoundTag;
 
-// Size : 44
+// Size: 44
 class Container {
-public: 
-	//vtable for Container	// 0
-	char filler1[40];		// 4
-
+public:
+	ContainerType type;				// 4-8
+	char containerfiller1[44 - 8];	// 8-44
+	
 	virtual ~Container();
-	virtual void addListener(ContainerListener *);
-	virtual void removeListener(ContainerListener *);
+	virtual void addListener(ContainerListener*);
+	virtual void removeListener(ContainerListener*);
 	virtual void getItem(int) const = 0;
-	virtual void addItem(ItemInstance *);
-	virtual void addItemToFirstEmptySlot(ItemInstance *);
-	virtual void setItem(int, ItemInstance *) = 0;
+	virtual void addItem(ItemInstance*);
+	virtual void addItemToFirstEmptySlot(ItemInstance*);
+	virtual void setItem(int, ItemInstance*) = 0;
 	virtual void removeItem(int, int) = 0;
-	virtual void getRandomEmptySlot(Random &);
-	virtual void dropContents(BlockSource &, Vec3 const &);
-	virtual void getName() const = 0;
-	virtual void getContainerSize() const = 0;
-	virtual void getMaxStackSize() const = 0;
-	virtual void startOpen(Player &) = 0;
-	virtual void stopOpen(Player &) = 0;
-	virtual void getSlotCopies() const;
-	virtual void getSlots();
+	virtual int getRandomEmptySlot(Random&);
+	virtual void dropContents(BlockSource&, const Vec3&);
+	virtual std::string getName() const = 0;
+	virtual int getContainerSize() const = 0;
+	virtual unsigned char getMaxStackSize() const = 0;
+	virtual void startOpen(Player&) = 0;
+	virtual void stopOpen(Player&) = 0;
+	virtual std::vector<ItemInstance> getSlotCopies() const;
+	virtual std::vector<ItemInstance*> getSlots();
 	virtual void getItemCount(int);
-	virtual void canPushInItem(int, int, ItemInstance *);
-	virtual void canPullOutItem(int, int, ItemInstance *);
+	virtual bool canPushInItem(int, int, ItemInstance*);
+	virtual bool canPullOutItem(int, int, ItemInstance*);
 	virtual void setContainerChanged(int);
 	virtual void setContainerMoved();
-	virtual void setCustomName(std::string const &);
-	virtual void hasCustomName() const;
-	virtual void readAdditionalSaveData(CompoundTag const &);
-	virtual void addAdditionalSaveData(CompoundTag &);
+	virtual void setCustomName(const std::string&);
+	virtual bool hasCustomName() const;
+	virtual void readAdditionalSaveData(const CompoundTag&);
+	virtual void addAdditionalSaveData(CompoundTag&);
+	
+	ContainerType getContainerType();
 };
---
+++ ContainerType.h
@@ -0,0 +1,11 @@
+#pragma once
+
+enum ContainerType : int {
+	None = 0,
+	CraftingContainer, // technically Anvils :P
+	Furnace,
+	BrewingStand = 4,
+	Dispenser = 6,
+	Hopper = 8
+	Cauldron,
+};
